2019dad694510be41b9cdd17c8702ab5627c17ba
Guillermo Silva <panahermoso10@gmail.com>
2024-04-26 18:58:52 -0400

    Se añadió una estructura llamada Lvl que representa el sistema de niveles.

--- Struct/main.cpp
+++ Struct/main.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+struct Lvl {
+    string palabra;
+    string consejo;
+    void print();
+};
+
 int main() {
-    cout << "Hello world" << endl;
+    Lvl w1;
+    w1.palabra = "Entero";
+    w1.consejo = "Sirven para xd";
+        
+    w1.print();
+    
+    return 0;
+}
+
+
+void Lvl::print() {
+    cout << palabra << ":" << endl;
+    cout << consejo << endl;
+
 }
