4fc7cd9c1860b5a3b0bf1c15ee68de15575a99f7
Guillermo Silva <panahermoso10@gmail.com>
2024-04-27 18:45:50 -0400

    Se  implementaron cinco niveles para el juego dentro de la funcion loadLevels().

--- Struct/main.cpp
+++ Struct/main.cpp
@@ -1,27 +1,59 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <queue>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 struct Lvl {
-    string palabra;
-    string consejo;
+    string word;
+    string advice;
     void print();
 };
 
+queue<Lvl> loadLevels();
+
 int main() {
-    Lvl w1;
-    w1.palabra = "Entero";
-    w1.consejo = "Sirven para xd";
-        
-    w1.print();
+    queue<Lvl> colaxd = loadLevels();
+
+    while (!colaxd.empty()) {
+        colaxd.front().print();
+        colaxd.pop();
+    }
     
+
     return 0;
 }
 
-
 void Lvl::print() {
-    cout << palabra << ":" << endl;
-    cout << consejo << endl;
+    cout << word << ":" << endl;
+    cout << advice << endl;
+}
 
+queue<Lvl> loadLevels() {
+    srand(time(0));
+    vector<Lvl> list;
+    queue<Lvl> levelQueue;
+
+    Lvl w1 = { "Entero" , "Sirven para..." };
+    Lvl w2 = { "Vector" , ".."};
+    Lvl w3 = { "Cola" , "..."};
+    Lvl w4 = { "Pila" , "...."};
+    Lvl w5 = { "Arreglo" , "....."};
+
+    list.push_back(w1);
+    list.push_back(w2);
+    list.push_back(w3);
+    list.push_back(w4);
+    list.push_back(w5);
+
+    
+    while(!list.empty()) {
+        int index = rand() % list.size();
+        levelQueue.push(list[index]);
+        list.erase(next(list.begin(),index));
+    }
+
+    return levelQueue;
 }
